ecaf52a3fc39ca47c6e723c385a6b63550a01bab
akter_rakib <rakibccj@gmail.com>
2020-12-15 11:35:48 +0600

    initiate project

---
+++ src/test/java/com/rakib/springbootpro/SpringBootProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.springbootpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
