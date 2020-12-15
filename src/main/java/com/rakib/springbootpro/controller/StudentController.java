ccebfbab09f4e0bebdb73c77341e17bf023cbc43
akter_rakib <rakibccj@gmail.com>
2020-12-15 11:44:14 +0600

    Custom ApiController annotation developed and use

--- src/main/java/com/rakib/springbootpro/controller/StudentController.java
+++ src/main/java/com/rakib/springbootpro/controller/StudentController.java
@@ -1,8 +1,14 @@
 package com.rakib.springbootpro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello Rakib ! Start From 0";
+    }
 
 }
