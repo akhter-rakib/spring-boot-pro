0b249e0f7884982310ff14a18880a34242d46b7e
akter_rakib <rakibccj@gmail.com>
2020-12-18 02:36:16 +0600

    inject Properties getting from application properties files

--- src/main/java/com/rakib/springbootpro/controller/StudentController.java
+++ src/main/java/com/rakib/springbootpro/controller/StudentController.java
@@ -1,10 +1,21 @@
 package com.rakib.springbootpro.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+    /*Inject Properties*/
+    @Value("${team.name}")
+    private String teamName;
+    @Value("${captain.name}")
+    private String captainName;
+
+    @GetMapping("/teamInfo")
+    public String teamInfo() {
+        return "Team Name : " + teamName + " Captain Name  :  " + captainName;
+    }
 
     @GetMapping("/hello")
     public String greeting() {
